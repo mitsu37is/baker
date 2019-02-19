4996132d01fb03c8ec45781a1394df8eda8ddfb7
Hm | edihasam <masa.ishi.right@gmail.com>
2019-02-20 07:17:11 +0900

    [create] actor / anime / character index

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,28 @@
 class ActorsController < ApplicationController
+  before_action :set_actor, only: %i[show update]
   def index
     @actors = Actor.all
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @actor.update(actor_params)
+      redirect_to @actor, notice: 'successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_actor
+    @actor = Actor.find(params[:id])
+  end
+
+  def actor_params
+    params.require(:actor).permit(:name, :sh_name)
   end
 end
--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,7 +1,15 @@
 class AnimesController < ApplicationController
+  before_action :set_anime, only: %i[show]
+
   def index
+    @animes = Anime.all
   end
 
-  def show
+  def show; end
+
+  private
+
+  def set_anime
+    @anime = Anime.find(params[:id])
   end
 end
