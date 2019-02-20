0d7eec7f33c3a93462a4a6ae6a1169c9780f8841
Hm | edihasam <masa.ishi.right@gmail.com>
2019-02-20 13:08:18 +0900

    load many data

--- db/seeds/anime_character.rb
+++ db/seeds/anime_character.rb
@@ -2,13 +2,18 @@
 
 require 'csv'
 
-actor_names = ['小野 大輔', '神谷 浩史', '花澤 香菜', '梶 裕貴']
+actor_names = CSV.read('db/csvs/actor_names.csv')[0]
 actor_names.each do |actor_name|
-  CSV.foreach("db/csvs/pixiv_actor_#{actor_name.gsub(" ", "")}.csv") do |row|
-    character_name = row[0]
-    anime_title = row[1]
+  puts actor_name
+  if File.exist?("db/csvs/pixiv_actor_#{actor_name.delete(' ')}.csv")
+    CSV.foreach("db/csvs/pixiv_actor_#{actor_name.delete(' ')}.csv") do |row|
+      character_name = row[0]
+      anime_title = row[1]
 
-    Character.find_or_create_by(name: character_name)
-    Anime.find_or_create_by(title: anime_title)
+      Character.find_or_create_by(name: character_name)
+      Anime.find_or_create_by(title: anime_title)
+    end
+  else
+    next
   end
 end
--- db/seeds/rel_actor_chara_and_anime.rb
+++ db/seeds/rel_actor_chara_and_anime.rb
@@ -1,18 +1,23 @@
 require 'csv'
 
-actor_names = ['小野 大輔', '神谷 浩史', '花澤 香菜', '梶 裕貴']
+actor_names = CSV.read('db/csvs/actor_names.csv')[0]
 
 actor_names.each do |actor_name|
   actor = Actor.find_by(name: actor_name)
 
-  CSV.foreach("db/csvs/pixiv_actor_#{actor_name.gsub(' ', '')}.csv") do |row|
-    character_name = row[0]
-    anime_title = row[1]
+  if File.exist?("db/csvs/pixiv_actor_#{actor_name.delete(' ')}.csv")
+    puts actor_name
+    CSV.foreach("db/csvs/pixiv_actor_#{actor_name.gsub(' ', '')}.csv") do |row|
+      character_name = row[0]
+      anime_title = row[1]
 
-    character = Character.find_or_create_by(name: character_name)
-    anime = Anime.find_or_create_by(title: anime_title)
+      character = Character.find_or_create_by(name: character_name)
+      anime = Anime.find_or_create_by(title: anime_title)
 
-    ActorCharacter.find_or_create_by(actor_id: actor.id, character_id: character.id)
-    AnimeCharacter.find_or_create_by(anime_id: anime.id, character_id: character.id)
+      ActorCharacter.find_or_create_by(actor_id: actor.id, character_id: character.id)
+      AnimeCharacter.find_or_create_by(anime_id: anime.id, character_id: character.id)
+    end
+  else
+    next
   end
 end
