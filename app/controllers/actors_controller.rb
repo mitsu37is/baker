ed7f88af7693d744cb7c2a62a3293f80d95eb30f
Hm | edihasam <masa.ishi.right@gmail.com>
2019-02-24 21:51:15 +0900

    [add]delete actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,5 @@
 class ActorsController < ApplicationController
-  before_action :set_actor, only: %i[show]
+  before_action :set_actor, only: %i[show edit update destroy]
   def index
     @q = Actor.ransack(params[:q])
     @actors = @q.result(distinct: true).page(params[:page])
@@ -7,9 +7,28 @@ class ActorsController < ApplicationController
 
   def show; end
 
+  def edit; end
+
+  def update
+    if @actor.update(actor_params)
+      redirect_to @actor, notice: 'successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @actor.destroy
+    redirect_to root_path
+  end
+
   private
 
   def set_actor
     @actor = Actor.find(params[:id])
+  end
+
+  def actor_params
+    params.require(:actor).permit(:name)
   end
 end
--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,6 @@
 class Actor < ApplicationRecord
   validates :sh_name, presence: true
 
-  has_many :actor_characters
+  has_many :actor_characters, dependent: :destroy
   has_many :characters, through: :actor_characters
 end
